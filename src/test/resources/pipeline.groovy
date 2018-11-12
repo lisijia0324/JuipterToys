65b58e986ad25374ddd7be602d3b67b8a8c3e85f
lisaLi <lisijia0324@gmail.com>
2018-11-12 14:23:31 +1100

    Add pipeline

---
+++ src/test/resources/pipeline.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    agent any & lt
+}
+tools{
+maven 'maven 3'
+jdk 'java 8'
+}
+
+stages {
+    stage("initialize") {
+        steps {
+            sh '''
+    echo "PATH = ${PATH}"
+    echo "M2_HOME = ${M2_HOME}"
+    '''
+        }
+    }
+}
